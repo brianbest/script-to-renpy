10c89534b109a441320af03fcb16aebb9120d26a
Brian Best <brian.brianb64@gmail.com>
2023-07-03 23:49:30 -0400

    it works! but the ui - it bad

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import ScriptToRenPy from "./script-to-renpy";
 
-console.log('Hello World', ScriptToRenPy);
+let scriptToRenPy = new ScriptToRenPy();
+
+let getForm = document.getElementById("script-form");
+
+getForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let script = document.getElementById("script").value;
+    scriptToRenPy.setScript(script);
+    document.getElementById("renpy-output").innerText = scriptToRenPy.getRenPyOutput();
+});
